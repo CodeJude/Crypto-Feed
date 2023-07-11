16a3e9d78b4cb02f88d74b92db6e992c2eabd41f
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-07-12 02:51:07 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import "./index.css";
 
 function App() {
   const [coins, setCoins] = useState([]);
-  const coinNumber = 20;
+  const coinNumber = 40;
   const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${coinNumber}&page=1&sparkline=false`;
 
   useEffect(() => {
